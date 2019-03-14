6d0d24c406c0b8a142459f225d888ce1ca29c384
root <root@localhost.localdomain>
2019-03-14 17:22:02 +0200

    v.2

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible">
+    <title>Test</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<h2>Test</h2>
 <?php
- echo("Ok");
- 
-?>
+    print "ok";
+ ?>
+</body>
+</html>
